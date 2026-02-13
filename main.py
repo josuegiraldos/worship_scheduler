0aeee62e8127939e24ad9c32719280b393768a2b
josuegiraldos <jggiraldosuarez2411@gmail.com>
2026-02-13 11:28:57 -0500

    main.py optimizado

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from src.scheduler import crear_cronograma
 from src.database import generar_texto_whatsapp, guardar_en_excel_maestro
+from src.analisis import obtener_conteo_mensual, generar_grafica_reporte
+from src.interface import menu_reportes
 
-# Definición de los servicios de la semana
-servicios_semana = ["Jueves", "Sabado", "Dominical", "Domingo tarde"]
+def main():
+    # Definición de los servicios de la semana
+    servicios_semana = ["Jueves", "Sabado", "Dominical", "Domingo tarde"]
 
-# 1. Generar el cronograma (incluye lógica de memoria y rescate de músicos)
-df_resultado = crear_cronograma(servicios_semana)
+    # 1. Generar el cronograma (incluye lógica de memoria y rescate de músicos)
+    df_resultado = crear_cronograma(servicios_semana)
 
-# 2. Generar salidas (WhatsApp y Excel Histórico)
-generar_texto_whatsapp(df_resultado)
-guardar_en_excel_maestro(df_resultado)
+    # 2. Generar salidas (WhatsApp y Excel Histórico)
+    generar_texto_whatsapp(df_resultado)
+    guardar_en_excel_maestro(df_resultado)
 
-# 3. Mostrar resultado en consola
-print("\n--- CRONOGRAMA DE ALABANZA ---")
-print(df_resultado)
+    # 3. Mostrar resultado en consola
+    # print(df_resultado)
+    
+    print("--- BIENVENIDO AL WORSHIP SCHEDULER ---")
+    menu_reportes()
+
+if __name__ == "__main__":
+    main()
